e0a87c618a227a766f205a59c07ae1d0e8239d22
Dan Gilbert <danielcgilbert@gmail.com>
2015-09-17 13:28:55 -0500

    Make compatible with #mount_uploaders.
    * Note, this makes the library incompatible with #mount_uploader.

--- lib/backgrounder/workers/process_asset.rb
+++ lib/backgrounder/workers/process_asset.rb
@@ -9,7 +9,7 @@ module CarrierWave
 
         if record && record.send(:"#{column}").present?
           record.send(:"process_#{column}_upload=", true)
-          if record.send(:"#{column}").recreate_versions! && record.respond_to?(:"#{column}_processing")
+          if record.send(:"#{column}").all?(&:recreate_versions!) && record.respond_to?(:"#{column}_processing")
             record.update_attribute :"#{column}_processing", false
           end
         end
